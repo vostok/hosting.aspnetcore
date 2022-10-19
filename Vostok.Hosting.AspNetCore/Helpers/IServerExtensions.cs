e7495cacb5027034fe281798c4d4ee99cf84e1dd
kungurtsev <kungurtsev@skbkontur.ru>
2022-10-19 19:47:02 +0400

    extract ServiceBeaconHostedService

---
+++ Vostok.Hosting.AspNetCore/Helpers/IServerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace Vostok.Hosting.Aspnetcore.Helpers;
+
+internal static class IServerExtensions
+{
+    public static ICollection<string> TryGetAddresses(this IServer server)
+    {
+        var addressFeature = server.Features.Get<IServerAddressesFeature>();
+        return addressFeature?.Addresses;
+    }
+}
