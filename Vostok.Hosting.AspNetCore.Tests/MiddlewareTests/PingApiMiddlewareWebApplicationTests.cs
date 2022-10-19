2408a475923691aed87a8ae0a5ad9f1551a2151a
Anna Teneneva <nddker@gmail.com>
2022-10-19 13:34:19 +0400

    Reuse tests from vostok.applications.aspnetcore lib

---
+++ Vostok.Hosting.AspNetCore.Tests/MiddlewareTests/PingApiMiddlewareWebApplicationTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using Vostok.Applications.AspNetCore.Tests;
+using Vostok.Applications.AspNetCore.Tests.Tests;
+using Vostok.Hosting.Setup;
+
+namespace Vostok.Hosting.AspNetCore.Tests.MiddlewareTests;
+
+[TestFixture(true)]
+public class PingApiMiddlewareWebApplicationTests : PingApiMiddlewareTests
+{
+    public PingApiMiddlewareWebApplicationTests(bool webApplication)
+        : base(webApplication)
+    {
+    }
+    
+    protected override IApplicationRunner CreateRunner(VostokHostingEnvironmentSetup setup)
+        => new WebApplicationRunner(setup, SetupGlobal);    
+}
