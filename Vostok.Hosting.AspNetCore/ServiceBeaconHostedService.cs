c44dee8ab839641c9cf0d7c99f295f180098e8b7
kungurtsev <kungurtsev@skbkontur.ru>
2022-10-24 17:37:53 +0400

    copy all code from host

--- Vostok.Hosting.AspNetCore/ServiceBeaconHostedService.cs
+++ Vostok.Hosting.AspNetCore/ServiceBeaconHostedService.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Vostok.Commons.Time;
+using Microsoft.Extensions.Options;
 using Vostok.Hosting.AspNetCore.Extensions;
 using Vostok.Logging.Abstractions;
 using Vostok.ServiceDiscovery;
@@ -22,42 +22,67 @@ internal class ServiceBeaconHostedService : IHostedService
     private readonly IServer server;
     private readonly IConfiguration configuration;
     private readonly ILog log;
+    private readonly VostokSettings settings;
 
-    public ServiceBeaconHostedService(IHostApplicationLifetime applicationLifetime, IServiceBeacon serviceBeacon, IServer server, IConfiguration configuration, ILog log)
+    public ServiceBeaconHostedService(IHostApplicationLifetime applicationLifetime, IServiceBeacon serviceBeacon, IServer server, IConfiguration configuration, ILog log, IOptions<VostokSettings> settings)
     {
         this.applicationLifetime = applicationLifetime;
         this.serviceBeacon = serviceBeacon;
         this.server = server;
         this.configuration = configuration;
         this.log = log.ForContext<ServiceBeaconHostedService>();
+        this.settings = settings.Value;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
         ValidateAddresses();
 
-        applicationLifetime.ApplicationStarted.Register(OnStartedAsync);
+        applicationLifetime.ApplicationStarted.Register(OnStarted);
+        applicationLifetime.ApplicationStopping.Register(OnStopping);
 
         return Task.CompletedTask;
     }
 
-    public async Task StopAsync(CancellationToken cancellationToken)
+    public Task StopAsync(CancellationToken cancellationToken)
     {
-        // todo (kungurtsev, 19.10.2022): 
+        return Task.CompletedTask; 
     }
 
-    private void OnStartedAsync()
+    private void OnStarted()
     {
         serviceBeacon.Start();
 
-        if (serviceBeacon is ServiceBeacon convertedBeacon)
-        {
-            convertedBeacon.WaitForInitialRegistrationAsync()
-                .WaitAsync(10.Seconds())
-                .ConfigureAwait(false);
-        }
+        WaitForServiceBeaconRegistrationIfNeeded();
+
+        // todo (kungurtsev, 24.10.2022): 
+        // if (settings.DiagnosticMetricsEnabled)
+        // {
+        //     if (environment.Diagnostics.HealthTracker is HealthTracker healthTracker)
+        //         healthTracker.LaunchPeriodicalChecks(environment.ShutdownToken);
+        //     else
+        //         log.Warn($"Provided {nameof(IHealthTracker)} instance is of unknown type {environment.Diagnostics.HealthTracker.GetType().Name}, unable to launch periodical health checks.");
+        // }
+
+        // todo (kungurtsev, 24.10.2022): 
+        // if (settings.SendAnnotations)
+        //     AnnotationsHelper.ReportInitialized(environment.ApplicationIdentity, environment.Metrics.Instance);
     }
 
+    private void OnStopping()
+    {
+        serviceBeacon.Stop();
+    }
+
+    private void WaitForServiceBeaconRegistrationIfNeeded()
+    {
+        if (!serviceBeacon.ReplicaInfo.TryGetUrl(out _) || !settings.BeaconRegistrationWaitEnabled || serviceBeacon is not ServiceBeacon convertedBeacon)
+            return;
+
+        if (!convertedBeacon.WaitForInitialRegistrationAsync().Wait(settings.BeaconRegistrationTimeout))
+            throw new Exception($"Service beacon hasn't registered in '{settings.BeaconRegistrationTimeout}'.");
+    }
+    
     private void ValidateAddresses()
     {
         var addresses = server.TryGetAddresses();
---
+++ Vostok.Hosting.AspNetCore/VostokSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Vostok.Commons.Time;
+using Vostok.Hosting.Abstractions;
+using Vostok.Hosting.Components.ThreadPool;
+
+namespace Vostok.Hosting.AspNetCore;
+
+[PublicAPI]
+public class VostokSettings
+{
+    /// <inheritdoc cref="VostokHostSettings.ConfigureStaticProviders"/>
+    public bool ConfigureStaticProviders { get; set; } = true;
+
+    /// <inheritdoc cref="VostokHostSettings.ConfigureThreadPool"/>
+    public bool ConfigureThreadPool { get; set; } = true;
+
+    /// <inheritdoc cref="VostokHostingEnvironmentWarmupSettings"/>
+    public VostokHostingEnvironmentWarmupSettings EnvironmentWarmupSettings { get; set; } = new();
+    
+    /// <inheritdoc cref="VostokHostSettings.SendAnnotations"/>
+    public bool SendAnnotations { get; set; } = true;
+
+    /// <inheritdoc cref="VostokHostSettings.DiagnosticMetricsEnabled"/>
+    public bool DiagnosticMetricsEnabled { get; set; } = true;
+
+    /// <inheritdoc cref="VostokHostSettings.BeaconRegistrationWaitEnabled"/>
+    public bool BeaconRegistrationWaitEnabled { get; set; }
+
+    /// <inheritdoc cref="VostokHostSettings.BeaconRegistrationTimeout"/>
+    public TimeSpan BeaconRegistrationTimeout { get; set; } = 10.Seconds();
+
+    /// <inheritdoc cref="VostokHostSettings.DisposeComponentTimeout"/>
+    public TimeSpan DisposeComponentTimeout { get; set; } = 5.Seconds();
+
+    /// <inheritdoc cref="VostokHostSettings.ThreadPoolTuningMultiplier"/>
+    public int ThreadPoolTuningMultiplier { get; set; } = ThreadPoolConstants.DefaultThreadPoolMultiplier;
+
+    /// <inheritdoc cref="VostokHostSettings.ThreadPoolSettingsProvider"/>
+    public Func<IConfigurationProvider, ThreadPoolSettings>? ThreadPoolSettingsProvider { get; set; }
+    
+    /// <inheritdoc cref="VostokHostSettings.BeforeInitializeApplication"/>
+    public List<Action<IVostokHostingEnvironment>> BeforeInitializeApplication { get; set; } = new();
+}
