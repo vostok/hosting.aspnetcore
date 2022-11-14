dc146390b52829c4ee2f13a74d8a9c0ab79bb090
kungurtsev <kungurtsev@skbkontur.ru>
2022-11-14 16:46:49 +0400

    dispose env

--- Vostok.Hosting.AspNetCore/Helpers/VostokHostingEnvironmentKeeper.cs
+++ Vostok.Hosting.AspNetCore/Helpers/VostokHostingEnvironmentKeeper.cs
@@ -1,11 +1,15 @@
-﻿using Vostok.Hosting.Abstractions;
+﻿using System;
+using Vostok.Hosting.Abstractions;
 
 namespace Vostok.Hosting.AspNetCore.Helpers;
 
-internal class VostokHostingEnvironmentKeeper
+internal class VostokHostingEnvironmentKeeper : IDisposable
 {
     public IVostokHostingEnvironment Environment { get; }
 
     public VostokHostingEnvironmentKeeper(IVostokHostingEnvironment environment) =>
         Environment = environment;
+
+    public void Dispose() =>
+        (Environment as IDisposable)?.Dispose();
 }
