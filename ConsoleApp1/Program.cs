41f3432f5d7e3f701cc882d507a8faf256495278
kungurtsev <kungurtsev@skbkontur.ru>
2022-11-23 16:15:08 +0400

    add console test app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1;
+using Vostok.Hosting.AspNetCore;
+using Vostok.Hosting.Kontur;
+using Vostok.Hosting.Setup;
+using Vostok.Logging.File.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddVostok(SetupVostok);
+
+builder.Services.AddHostedService<Worker>();
+
+var app = builder.Build();
+
+app.Run();
+
+void SetupVostok(IVostokHostingEnvironmentBuilder builder)
+{
+    builder.SetupApplicationIdentity(identity =>
+    {
+        identity.SetProject("Vostok");
+        identity.SetApplication("TestAspNetCoreConsoleHosting");
+        identity.SetEnvironment("dev");
+    });
+
+    builder.SetupLog(log =>
+    {
+        log.SetupConsoleLog();
+        log.SetupFileLog(fileLog => fileLog.CustomizeSettings(
+            fileLogSettings => fileLogSettings.FileOpenMode = FileOpenMode.Rewrite));
+    });
+    
+    builder.SetupForKontur();
+}
