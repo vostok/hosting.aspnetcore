79b624ca00e9dd9f094ba7c8f33bcfd5bf90c81a
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-08 19:04:53 +0400

    clean up HoustonHostedService

--- Vostok.Hosting.AspNetCore.Houston/HoustonHostedService.cs
+++ Vostok.Hosting.AspNetCore.Houston/HoustonHostedService.cs
@@ -12,13 +12,11 @@ namespace Vostok.Hosting.AspNetCore.Houston;
 
 internal class HoustonHostedService : IHostedService
 {
-    private readonly HoustonContext? context;
     private readonly IVostokHostingEnvironment environment;
     private readonly List<Action<IVostokHostingEnvironment>> actions;
 
     public HoustonHostedService(HoustonContext? context, IVostokHostingEnvironment environment, List<Action<IVostokHostingEnvironment>> actions, VostokApplicationStateObservable applicationStateObservable, IHostApplicationLifetime applicationLifetime)
     {
-        this.context = context;
         this.environment = environment;
         this.actions = actions;
 
@@ -37,14 +35,6 @@ internal class HoustonHostedService : IHostedService
         return Task.CompletedTask;
     }
 
-    public async Task StopAsync(CancellationToken cancellationToken)
-    {
-        // todo (kungurtsev, 28.11.2022): pass status
-        
-        if (context == null)
-            return;
-
-        // todo (kungurtsev, 30.11.2022): should it be here?
-        // await context.Shutdown.HandleStop(new VostokApplicationRunResult(VostokApplicationState.Stopped));
-    }
+    public Task StopAsync(CancellationToken cancellationToken) =>
+        Task.CompletedTask;
 }
