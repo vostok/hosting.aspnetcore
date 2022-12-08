dbdbc9c207d7581ca2147ddcad9ecfd1f16a18ae
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-08 14:10:33 +0400

    setup configuration, create env on add

--- Vostok.Hosting.AspNetCore/AddVostokExtensions.cs
+++ Vostok.Hosting.AspNetCore/AddVostokExtensions.cs
@@ -1,10 +1,10 @@
-using System;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Vostok.Applications.AspNetCore.Helpers;
 using Vostok.Commons.Threading;
+using Vostok.Hosting.Abstractions;
 using Vostok.Hosting.AspNetCore.Extensions;
 using Vostok.Hosting.Components.Shutdown;
 using Vostok.Hosting.Setup;
@@ -17,61 +17,66 @@ public static class AddVostokExtensions
     public static void AddVostok(
         this WebApplicationBuilder webApplicationBuilder,
         VostokHostingEnvironmentSetup environmentSetup,
-        Action<VostokComponentsSettings>? componentsSettingsSetup = null) =>
-        webApplicationBuilder.Services.AddVostok(environmentSetup, componentsSettingsSetup);
-    
+        VostokComponentsSettings? settings = null)
+    {
+        var environment = CreateEnvironment(environmentSetup, settings);
+
+        webApplicationBuilder.Configuration.AddVostokSources(environment);
+        
+        webApplicationBuilder.Services.AddVostok(environment);
+    }
+
     public static void AddVostok(
         this IHostBuilder hostBuilder,
         VostokHostingEnvironmentSetup environmentSetup,
-        Action<VostokComponentsSettings>? componentsSettingsSetup = null) =>
-        hostBuilder.ConfigureServices(serviceCollection => 
-            serviceCollection.AddVostok(environmentSetup, componentsSettingsSetup));
-    
-    public static void AddVostok(
-        this IServiceCollection serviceCollection,
-        VostokHostingEnvironmentSetup environmentSetup,
-        Action<VostokComponentsSettings>? componentsSettingsSetup = null)
+        VostokComponentsSettings? settings = null)
     {
-        if (componentsSettingsSetup != null)
-            serviceCollection.Configure(componentsSettingsSetup);
+        var environment = CreateEnvironment(environmentSetup, settings);
 
-        serviceCollection.AddSingleton(services =>
+        hostBuilder.ConfigureAppConfiguration(config => config.AddVostokSources(environment));
+
+        hostBuilder.ConfigureServices(serviceCollection =>
         {
-            var settings = services.GetFromOptionsOrDefault<VostokComponentsSettings>();
-
-            var environmentFactorySettings = new VostokHostingEnvironmentFactorySettings
-            {
-                ConfigureStaticProviders = settings.ConfigureStaticProviders,
-                DisposeComponentTimeout = settings.DisposeComponentTimeout,
-                SendAnnotations = settings.SendAnnotations,
-                DiagnosticMetricsEnabled = settings.DiagnosticMetricsEnabled,
-                SetupShutdownSupported = false
-            };
-
-            // review: Looks like this code is also included in VostokHostedService.ConfigureHostBeforeRun. Is it intentional?
-            // cr (kungurtsev, 23.11.2022): yes, here we tune it with defaults without knowledge of cpu limits
-            if (settings.ConfigureThreadPool)
-                ThreadPoolUtility.Setup(settings.ThreadPoolTuningMultiplier);
-
-            var environment = VostokHostingEnvironmentFactory.Create(
-                environmentSetup,
-                environmentFactorySettings);
-
-            return environment;
+            serviceCollection.AddVostok(environment);
         });
+    }
+
+    private static void AddVostok(this IServiceCollection serviceCollection, IVostokHostingEnvironment environment)
+    {
+        serviceCollection.AddSingleton(environment);
 
         serviceCollection.AddVostokEnvironmentComponents();
         serviceCollection.AddOnApplicationStateChanged();
         serviceCollection.AddVostokLoggerProvider();
 
-        // review: Put my thought in "WebApplication1" :) will also try to think about possible solution
-        // todo (kungurtsev, 14.11.2022): deal with configuration
-
         serviceCollection.AddHostedService<VostokHostedService>();
         serviceCollection.AddHostedService<ServiceBeaconHostedService>();
 
         serviceCollection.AddHealthChecks();
-        
+
         serviceCollection.ConfigureShutdownTimeout(ShutdownConstants.DefaultShutdownTimeout);
+    }
+
+    private static IVostokHostingEnvironment CreateEnvironment(VostokHostingEnvironmentSetup environmentSetup, VostokComponentsSettings? settings)
+    {
+        settings ??= new VostokComponentsSettings();
+
+        var environmentFactorySettings = new VostokHostingEnvironmentFactorySettings
+        {
+            ConfigureStaticProviders = settings.ConfigureStaticProviders,
+            DisposeComponentTimeout = settings.DisposeComponentTimeout,
+            SendAnnotations = settings.SendAnnotations,
+            DiagnosticMetricsEnabled = settings.DiagnosticMetricsEnabled,
+            SetupShutdownSupported = false
+        };
+
+        if (settings.ConfigureThreadPool)
+            ThreadPoolUtility.Setup(settings.ThreadPoolTuningMultiplier);
+
+        var environment = VostokHostingEnvironmentFactory.Create(
+            environmentSetup,
+            environmentFactorySettings);
+        
+        return environment;
     }
 }
