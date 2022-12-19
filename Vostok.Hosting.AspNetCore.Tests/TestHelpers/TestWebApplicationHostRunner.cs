e4f16ab20cb5b3d97ba5ee122ab79e13430e0e7f
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-19 13:23:32 +0400

    fix build after code movements

--- Vostok.Hosting.AspNetCore.Tests/TestHelpers/TestWebApplicationHostRunner.cs
+++ Vostok.Hosting.AspNetCore.Tests/TestHelpers/TestWebApplicationHostRunner.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Vostok.Applications.AspNetCore.Tests;
 using Vostok.Applications.AspNetCore.Tests.Extensions;
+using Vostok.Applications.AspNetCore.Tests.TestHelpers;
 using Vostok.Hosting.AspNetCore.Extensions;
 using Vostok.Hosting.Setup;
 
-namespace Vostok.Hosting.AspNetCore.Tests;
+namespace Vostok.Hosting.AspNetCore.Tests.TestHelpers;
 
 public class TestWebApplicationHostRunner : ITestHostRunner
 {
