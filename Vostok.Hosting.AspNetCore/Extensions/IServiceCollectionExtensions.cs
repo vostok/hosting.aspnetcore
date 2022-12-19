2fa8ce0c33714cd54deb49fcccf1d2d8cc047424
Konstantin Chibasov <koheatel@gmail.com>
2022-12-19 16:16:51 +0600

    move complex middleware configuration code to separate extension class

--- Vostok.Hosting.AspNetCore/Extensions/IServiceCollectionExtensions.cs
+++ Vostok.Hosting.AspNetCore/Extensions/IServiceCollectionExtensions.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using JetBrains.Annotations;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Vostok.Applications.AspNetCore;
-using Vostok.Applications.AspNetCore.Configuration;
-using Vostok.Applications.AspNetCore.Diagnostics;
-using Vostok.Hosting.Abstractions;
-using Vostok.Hosting.Abstractions.Diagnostics;
 using Vostok.Logging.Abstractions;
 using Vostok.Logging.Microsoft;
-using Vostok.Throttling;
-using Vostok.Throttling.Metrics;
 
 namespace Vostok.Hosting.AspNetCore.Extensions;
 
@@ -31,16 +26,15 @@ public static class IServiceCollectionExtensions
         services
             .Configure(builder.DiagnosticFeatures)
             .Configure(builder.Throttling)
-            .AddThrottlingProvider()
-            .AddRequestTracking();
+            .ConfigureKestrelDefaults();
 
         return services
+            .AddRequestTracking()
             .AddVostokHttpContextTweaks(builder.HttpContextTweaks)
             .AddVostokRequestInfo(builder.FillRequestInfo)
             .AddVostokDistributedContext(builder.DistributedContext)
             .AddVostokTracing(builder.Tracing)
-            // .AddVostokThrottling(null, builder.ThrottlingMiddleware) // todo: overload without IThrottlingProvider
-            .Configure(builder.ThrottlingMiddleware)
+            .AddThrottling(builder.ThrottlingMiddleware)
             .AddVostokRequestLogging(builder.RequestLogging)
             .AddVostokDatacenterAwareness(builder.DatacenterAwareness)
             .AddVostokUnhandledExceptions(builder.UnhandledExceptions)
@@ -59,84 +53,16 @@ public static class IServiceCollectionExtensions
             new VostokLoggerProvider(serviceProvider.GetRequiredService<ILog>(), serviceProvider.GetFromOptionsOrDefault<VostokLoggerProviderSettings>())));
     }
 
-    private static IServiceCollection AddThrottlingProvider(this IServiceCollection services)
+    private static IServiceCollection ConfigureKestrelDefaults(this IServiceCollection services)
     {
-        return services.AddSingleton<IThrottlingProvider>(sp =>
+        return services.Configure<KestrelServerOptions>(s =>
         {
-            var throttlingProvider = sp.GetFromOptionsOrDefault<VostokThrottlingSettings>().BuildProvider();
-            var diagnosticSettings = sp.GetFromOptionsOrDefault<DiagnosticFeaturesSettings>();
+            s.AddServerHeader = false;
 
-            AddThrottlingMetrics(sp, throttlingProvider);
-
-            if (diagnosticSettings.AddThrottlingInfoProvider)
-                AddThrottlingInfo(sp, throttlingProvider);
-
-            if (diagnosticSettings.AddThrottlingHealthCheck)
-                AddThrottlingHealthCheck(sp, throttlingProvider);
-
-            return throttlingProvider;
+            s.Limits.MaxRequestBufferSize = 128 * 1024;
+            s.Limits.MaxRequestLineSize = 32 * 1024;
+            s.Limits.MaxRequestHeadersTotalSize = 64 * 1024;
+            s.Limits.MaxConcurrentUpgradedConnections = 10000;
         });
-    }
-
-    private static void AddThrottlingMetrics(IServiceProvider serviceProvider, ThrottlingProvider throttlingProvider)
-    {
-        var metrics = serviceProvider.GetService<IVostokApplicationMetrics>();
-        var settings = serviceProvider.GetFromOptionsOrDefault<VostokThrottlingSettings>();
-
-        if (settings.Metrics == null || metrics == null)
-            return;
-
-        serviceProvider.RegisterDisposable(metrics.Instance.CreateThrottlingMetrics(throttlingProvider, settings.Metrics));
-    }
-
-    private static void AddThrottlingInfo(IServiceProvider serviceProvider, ThrottlingProvider throttlingProvider)
-    {
-        var diagnostics = serviceProvider.GetService<IVostokApplicationDiagnostics>();
-        if (diagnostics == null)
-            return;
-
-        var infoEntry = new DiagnosticEntry(DiagnosticConstants.Component, "request-throttling");
-        var infoProvider = new ThrottlingInfoProvider(throttlingProvider);
-
-        serviceProvider.RegisterDisposable(diagnostics.Info.RegisterProvider(infoEntry, infoProvider));
-    }
-
-    private static void AddThrottlingHealthCheck(IServiceProvider serviceProvider, ThrottlingProvider throttlingProvider)
-    {
-        var diagnostics = serviceProvider.GetService<IVostokApplicationDiagnostics>();
-        if (diagnostics == null)
-            return;
-
-        var healthCheck = new ThrottlingHealthCheck(throttlingProvider);
-
-        serviceProvider.RegisterDisposable(diagnostics.HealthTracker.RegisterCheck("Request throttling", healthCheck));
-    }
-
-    private static IServiceCollection AddRequestTracking(this IServiceCollection services)
-    {
-        return services.AddSingleton<IRequestTracker>(sp =>
-        {
-            var settings = sp.GetFromOptionsOrDefault<DiagnosticFeaturesSettings>();
-
-            if (!settings.AddCurrentRequestsInfoProvider)
-                return new DevNullRequestTracker();
-
-            var requestTracker = new RequestTracker();
-            AddRequestTrackingInfo(sp, requestTracker);
-
-            return requestTracker;
-        });
-    }
-
-    private static void AddRequestTrackingInfo(IServiceProvider serviceProvider, RequestTracker requestTracker)
-    {
-        var diagnostics = serviceProvider.GetService<IVostokApplicationDiagnostics>();
-        if (diagnostics == null)
-            return;
-
-        var infoEntry = new DiagnosticEntry(DiagnosticConstants.Component, "requests-in-progress");
-        var infoProvider = new CurrentRequestsInfoProvider(requestTracker);
-
-        serviceProvider.RegisterDisposable(diagnostics.Info.RegisterProvider(infoEntry, infoProvider));
     }
 }
