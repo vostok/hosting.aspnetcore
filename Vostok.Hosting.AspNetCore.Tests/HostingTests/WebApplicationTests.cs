0d0511354eeed92257670416fbc06de8b89e4717
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-21 17:14:53 +0400

    check warmup ping api, test with and without middlewares

--- Vostok.Hosting.AspNetCore.Tests/HostingTests/WebApplicationTests.cs
+++ Vostok.Hosting.AspNetCore.Tests/HostingTests/WebApplicationTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions.Extensions;
 using Microsoft.AspNetCore.Builder;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NUnit.Framework;
 using Vostok.Commons.Helpers.Network;
+using Vostok.Hosting.AspNetCore.Extensions;
 using Vostok.Hosting.AspNetCore.Tests.TestHelpers;
 using Vostok.Logging.Abstractions;
 using Vostok.ServiceDiscovery.Abstractions;
@@ -13,11 +15,23 @@ using Vostok.ServiceDiscovery.Abstractions.Models;
 
 namespace Vostok.Hosting.AspNetCore.Tests.HostingTests;
 
-[TestFixture]
+[TestFixture(false)]
+[TestFixture(true)]
 internal class WebApplicationTests
 {
+    private readonly bool middlewares;
     private CheckingLog checkingLog;
+    private string url;
 
+    public WebApplicationTests(bool middlewares) =>
+        this.middlewares = middlewares;
+
+    [SetUp]
+    public void SetUp()
+    {
+        url = $"http://localhost:{FreeTcpPortFinder.GetFreePort()}";
+    }
+    
     [TearDown]
     public void TearDown()
     {
@@ -28,12 +42,16 @@ internal class WebApplicationTests
     [Test]
     public async Task Should_start_and_stop_and_write_logs()
     {
+        var toDispose = middlewares ? 4 : 0;
         checkingLog = new CheckingLog(
             "[VostokApplicationStateObservable] New state: EnvironmentWarmup.",
             "[VostokHostingEnvironment] Registered host extensions:",
             "[VostokApplicationStateObservable] New state: Initializing.",
             "[ServiceBeaconHostedService] Using url provided in Service beacon:",
             "[Microsoft.Hosting.Lifetime] Now listening on:",
+            IfMiddlewares("[Warmup] Sending request 'GET _status/ping'"),
+            IfMiddlewares("[LoggingMiddleware] Received request 'GET /_status/ping' from 'Vostok.Test.SomeApplication'"),
+            IfMiddlewares("[Warmup] Success. Response code = 200 ('Ok')."),
             "[FakeServiceBeacon] Start.",
             "[VostokHostedService] Started.",
             "[VostokApplicationStateObservable] New state: Running.",
@@ -45,6 +63,7 @@ internal class WebApplicationTests
             "[Microsoft.Hosting.Lifetime] Application is shutting down...",
             "[VostokHostedService] Stopped.",
             "[VostokApplicationStateObservable] New state: Stopped.",
+            $"[VostokHostingEnvironment] Disposing of Disposables list ({toDispose} element(s))..",
             "[VostokHostingEnvironment] Disposing of VostokHostingEnvironment..",
             "[VostokHostingEnvironment] Disposing of FileLog.."
         );
@@ -54,17 +73,24 @@ internal class WebApplicationTests
         builder.UseVostokHosting(environmentBuilder =>
         {
             environmentBuilder.ApplyTestsDefaults();
+            environmentBuilder.SetupServiceBeacon(beacon => beacon.SetupReplicaInfo(info => info.SetEnvironment("default").SetApplication("test").SetUrl(new Uri(url))));
             environmentBuilder.SetupLog(logBuilder => logBuilder.AddLog(checkingLog));
         });
 
         builder.Services.AddSingleton<IServiceBeacon>(services => new FakeServiceBeacon(
-            new ReplicaInfo("default", "test", $"http://localhost:{FreeTcpPortFinder.GetFreePort()}"),
+            new ReplicaInfo("default", "test", url),
             services.GetRequiredService<ILog>()));
+
+        if (middlewares)
+            builder.Services.AddVostokMiddlewares();
 
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
 
+        if (middlewares)
+            app.UseVostokMiddlewares();
+        
         app.Start();
 
         Thread.Sleep(5.Seconds());
@@ -72,4 +98,7 @@ internal class WebApplicationTests
         await app.StopAsync();
         await app.DisposeAsync();
     }
+
+    private string? IfMiddlewares(string? messageYes = null, string? messageNo = null) =>
+        middlewares ? messageYes : messageNo;
 }
