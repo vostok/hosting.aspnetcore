130f991a9af190b834439d62a4de4df118f3e10b
Konstantin Chibasov <koheatel@gmail.com>
2022-12-22 14:15:19 +0600

    include cpu limits in throttling setup

--- Vostok.Hosting.AspNetCore/VostokThrottlingSettings.cs
+++ Vostok.Hosting.AspNetCore/VostokThrottlingSettings.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using JetBrains.Annotations;
-using Vostok.Throttling;
 using Vostok.Throttling.Config;
 using Vostok.Throttling.Metrics;
 using Vostok.Throttling.Quotas;
@@ -10,37 +9,26 @@ namespace Vostok.Hosting.AspNetCore;
 [PublicAPI]
 public class VostokThrottlingSettings
 {
-    private readonly ThrottlingConfigurationBuilder configurationBuilder = new();
+    internal readonly ThrottlingConfigurationBuilder ConfigurationBuilder = new();
 
     public bool UseThreadPoolOverloadQuota { get; set; } = true;
     public ThrottlingMetricsOptions? Metrics { get; set; } = new();
 
     public VostokThrottlingSettings UseEssentials(Func<ThrottlingEssentials> essentialsProvider)
     {
-        configurationBuilder.SetEssentials(essentialsProvider);
+        ConfigurationBuilder.SetEssentials(essentialsProvider);
         return this;
     }
 
     public VostokThrottlingSettings UsePropertyQuota(string propertyName, Func<PropertyQuotaOptions> quotaOptionsProvider)
     {
-        configurationBuilder.SetPropertyQuota(propertyName, quotaOptionsProvider);
+        ConfigurationBuilder.SetPropertyQuota(propertyName, quotaOptionsProvider);
         return this;
     }
 
     public VostokThrottlingSettings UseCustomQuota(IThrottlingQuota quota)
     {
-        configurationBuilder.AddCustomQuota(quota);
+        ConfigurationBuilder.AddCustomQuota(quota);
         return this;
-    }
-
-    internal ThrottlingProvider BuildProvider()
-    {
-        if (UseThreadPoolOverloadQuota)
-        {
-            var threadPoolQuota = new ThreadPoolOverloadQuota(new ThreadPoolOverloadQuotaOptions());
-            configurationBuilder.AddCustomQuota(threadPoolQuota);
-        }
-
-        return new ThrottlingProvider(configurationBuilder.Build());
     }
 }
