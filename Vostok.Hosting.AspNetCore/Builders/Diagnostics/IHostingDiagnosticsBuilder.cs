b1a588fb6ecb97415fab11acfb836812b06e79c1
Konstantin Chibasov <koheatel@gmail.com>
2022-12-23 11:36:43 +0600

    Add builder for diagnostics

---
+++ Vostok.Hosting.AspNetCore/Builders/Diagnostics/IHostingDiagnosticsBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Vostok.Applications.AspNetCore.Configuration;
+
+namespace Vostok.Hosting.AspNetCore.Builders.Diagnostics;
+
+public interface IHostingDiagnosticsBuilder
+{
+    IHostingDiagnosticsBuilder ConfigureMiddleware(Action<DiagnosticApiSettings> configure);
+    IHostingDiagnosticsBuilder ConfigureFeatures(Action<DiagnosticFeaturesSettings> configure);
+}
