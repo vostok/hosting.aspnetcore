5fd031626711a0d88896c7e419d567c5096aa46b
kungurtsev <kungurtsev@skbkontur.ru>
2022-12-28 13:33:39 +0400

    provide info from environment in middlewares

---
+++ Vostok.Hosting.AspNetCore/Helpers/BuiltVostokEnvironment.cs
@@ -0,0 +1,11 @@
+﻿using Vostok.Hosting.Abstractions;
+
+namespace Vostok.Hosting.AspNetCore.Helpers;
+
+internal class BuiltVostokEnvironment
+{
+    public BuiltVostokEnvironment(IVostokHostingEnvironment environment) =>
+        Environment = environment;
+
+    public IVostokHostingEnvironment Environment { get; }
+}
