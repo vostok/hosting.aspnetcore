9080529da6536b0f1b6028786f07d04767899816
kungurtsev <kungurtsev@skbkontur.ru>
2023-01-11 15:11:11 +0400

    simplify args name

--- Vostok.Hosting.AspNetCore/Web/Configuration/ThrottlingSettingsExtensions.cs
+++ Vostok.Hosting.AspNetCore/Web/Configuration/ThrottlingSettingsExtensions.cs
@@ -10,37 +10,37 @@ namespace Vostok.Hosting.AspNetCore.Web.Configuration;
 [PublicAPI]
 public static class ThrottlingSettingsExtensions
 {
-    public static ThrottlingSettings UseQuota(this ThrottlingSettings settings, string quotaName, Func<PropertyQuotaOptions> quotaOptionsProvider)
+    public static ThrottlingSettings UseQuota(this ThrottlingSettings settings, string name, Func<PropertyQuotaOptions> optionsProvider)
     {
-        settings.Quotas.Add(new ThrottlingQuota(quotaName, quotaOptionsProvider));
+        settings.Quotas.Add(new ThrottlingQuota(name, optionsProvider));
         return settings;
     }
 
-    public static ThrottlingSettings UseConsumerQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
-        settings.UseQuota(WellKnownThrottlingProperties.Consumer, quotaOptionsProvider);
+    public static ThrottlingSettings UseConsumerQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> optionsProvider) =>
+        settings.UseQuota(WellKnownThrottlingProperties.Consumer, optionsProvider);
 
-    public static ThrottlingSettings UsePriorityQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
-        settings.UseQuota(WellKnownThrottlingProperties.Priority, quotaOptionsProvider);
+    public static ThrottlingSettings UsePriorityQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> optionsProvider) =>
+        settings.UseQuota(WellKnownThrottlingProperties.Priority, optionsProvider);
 
-    public static ThrottlingSettings UseMethodQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
-        settings.UseQuota(WellKnownThrottlingProperties.Method, quotaOptionsProvider);
+    public static ThrottlingSettings UseMethodQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> optionsProvider) =>
+        settings.UseQuota(WellKnownThrottlingProperties.Method, optionsProvider);
 
-    public static ThrottlingSettings UseUrlQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
-        settings.UseQuota(WellKnownThrottlingProperties.Url, quotaOptionsProvider);
+    public static ThrottlingSettings UseUrlQuota(this ThrottlingSettings settings, Func<PropertyQuotaOptions> optionsProvider) =>
+        settings.UseQuota(WellKnownThrottlingProperties.Url, optionsProvider);
 
-    public static ThrottlingSettings UseEssentials(this ThrottlingSettings settings, Func<ThrottlingEssentials> essentialsProvider)
+    public static ThrottlingSettings UseEssentials(this ThrottlingSettings settings, Func<ThrottlingEssentials> optionsProvider)
     {
-        settings.Essentials = essentialsProvider;
+        settings.Essentials = optionsProvider;
         return settings;
     }
 
     public static ThrottlingSettings UseEssentials(this ThrottlingSettings settings, ThrottlingEssentials essentials) =>
         settings.UseEssentials(() => essentials);
 
-    public static ThrottlingSettings UseCustomPropertyQuota(this ThrottlingSettings settings, string propertyName, Func<HttpContext, string> propertyValueProvider, Func<PropertyQuotaOptions> quotaOptionsProvider)
+    public static ThrottlingSettings UseCustomPropertyQuota(this ThrottlingSettings settings, string name, Func<HttpContext, string> valueProvider, Func<PropertyQuotaOptions> optionsProvider)
     {
-        settings.Properties.Add(new ThrottlingProperty(propertyName, propertyValueProvider));
-        settings.UseQuota(propertyName, quotaOptionsProvider);
+        settings.Properties.Add(new ThrottlingProperty(name, valueProvider));
+        settings.UseQuota(name, optionsProvider);
         return settings;
     }
 }
