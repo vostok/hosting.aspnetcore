ba2a44bcfd1617a88ad8ad1332d380995e0092aa
kungurtsev <kungurtsev@skbkontur.ru>
2023-01-14 22:56:13 +0400

    fixing git

--- Vostok.Hosting.Aspnetcore/Web/Configuration/VostokThrottlingConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Vostok.Hosting.AspNetCore.Web.Configuration;
-
-internal class VostokThrottlingConfiguration
-{
-    public bool UseThreadPoolOverloadQuota { get; set; } = true;
-
-    public bool EnableMetrics { get; set; } = true;
-}
