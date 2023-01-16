0a24041f23a7f9aaa4382d48d6ef8e3e6c72a0de
kungurtsev <kungurtsev@skbkontur.ru>
2023-01-16 18:07:36 +0400

    better doc

--- Vostok.Hosting.AspNetCore/Web/Configuration/ThrottlingSettingsExtensions.cs
+++ Vostok.Hosting.AspNetCore/Web/Configuration/ThrottlingSettingsExtensions.cs
@@ -9,35 +9,50 @@ namespace Vostok.Hosting.AspNetCore.Web.Configuration;
 [PublicAPI]
 public static class ThrottlingSettingsExtensions
 {
+    /// <summary>
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseConsumerQuota"/>
+    /// Also call <see cref="ThrottlingConfigurationBuilderExtensions.UseConsumerQuota"/>.
+    /// </summary>
     public static ThrottlingSettings UseConsumerQuota(this ThrottlingSettings settings)
     {
         settings.AddConsumerProperty = true;
         return settings;
     }
     
+    /// <summary>
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UsePriorityQuota"/>
+    /// Also call <see cref="ThrottlingConfigurationBuilderExtensions.UsePriorityQuota"/>.
+    /// </summary>
     public static ThrottlingSettings UsePriorityQuota(this ThrottlingSettings settings)
     {
         settings.AddPriorityProperty = true;
         return settings;
     }
     
+    /// <summary>
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseMethodQuota"/>
+    /// Also call <see cref="ThrottlingConfigurationBuilderExtensions.UseMethodQuota"/>.
+    /// </summary>
     public static ThrottlingSettings UseMethodQuota(this ThrottlingSettings settings)
     {
         settings.AddMethodProperty = true;
         return settings;
     }
     
+    /// <summary>
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseUrlQuota"/>
+    /// Also call <see cref="ThrottlingConfigurationBuilderExtensions.UseUrlQuota"/>.
+    /// </summary>
     public static ThrottlingSettings UseUrlQuota(this ThrottlingSettings settings)
     {
         settings.AddUrlProperty = true;
         return settings;
     }
     
+    /// <summary>
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseCustomPropertyQuota"/>
+    /// Also call <see cref="ThrottlingConfigurationBuilderExtensions.UseCustomPropertyQuota"/>.
+    /// </summary>
     public static ThrottlingSettings UseCustomPropertyQuota(this ThrottlingSettings settings, string propertyName, Func<HttpContext, string> propertyValueProvider)
     {
         settings.AdditionalProperties.Add(context => (propertyName, propertyValueProvider(context)));
