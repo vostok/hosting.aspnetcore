6f3846047d2560988a7b1eb9a8832dad37021c4b
kungurtsev <kungurtsev@skbkontur.ru>
2023-01-16 17:36:40 +0400

    + settings extensions

--- Vostok.Hosting.AspNetCore/Web/Configuration/IVostokThrottlingConfiguratorExtensions.cs
+++ Vostok.Hosting.AspNetCore/Web/Configuration/IVostokThrottlingConfiguratorExtensions.cs
@@ -2,7 +2,6 @@
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Vostok.Applications.AspNetCore.Builders;
-using Vostok.Throttling;
 using Vostok.Throttling.Config;
 using Vostok.Throttling.Quotas;
 
@@ -13,7 +12,7 @@ public static class IVostokThrottlingConfiguratorExtensions
 {
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.DisableThrottling"/>
     public static IVostokThrottlingConfigurator DisableThrottling(this IVostokThrottlingConfigurator configurator) => 
-        configurator.ConfigureMiddleware(s => s.Enabled = _ => false);
+        configurator.ConfigureMiddleware(settings => settings.Enabled = _ => false);
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.DisableMetrics"/>
     public static IVostokThrottlingConfigurator DisableMetrics(this IVostokThrottlingConfigurator configurator) => 
@@ -25,43 +24,43 @@ public static class IVostokThrottlingConfiguratorExtensions
     
     /// <inheritdoc cref="IVostokThrottlingBuilder.UseEssentials"/>
     public static IVostokThrottlingConfigurator UseEssentials(this IVostokThrottlingConfigurator configurator, Func<ThrottlingEssentials> essentialsProvider) =>
-        configurator.ConfigureBuilder(builder => builder.SetEssentials(essentialsProvider));
+        configurator.ConfigureBuilder(builder => builder.UseEssentials(essentialsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilder.UsePropertyQuota"/>
     public static IVostokThrottlingConfigurator UsePropertyQuota(this IVostokThrottlingConfigurator configurator, string propertyName, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
-        configurator.ConfigureBuilder(builder => builder.SetPropertyQuota(propertyName, quotaOptionsProvider));
+        configurator.ConfigureBuilder(builder => builder.UsePropertyQuota(propertyName, quotaOptionsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilder.UseCustomQuota"/>
     public static IVostokThrottlingConfigurator UseCustomQuota(this IVostokThrottlingConfigurator configurator, IThrottlingQuota quota) =>
-        configurator.ConfigureBuilder(builder => builder.AddCustomQuota(quota));
+        configurator.ConfigureBuilder(builder => builder.UseCustomQuota(quota));
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseConsumerQuota"/>
     public static IVostokThrottlingConfigurator UseConsumerQuota(this IVostokThrottlingConfigurator configurator, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
         configurator
-            .ConfigureMiddleware(settings => settings.AddConsumerProperty = true)
-            .UsePropertyQuota(WellKnownThrottlingProperties.Consumer, quotaOptionsProvider);
+            .ConfigureMiddleware(settings => settings.UseConsumerQuota())
+            .ConfigureBuilder(builder => builder.UseConsumerQuota(quotaOptionsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UsePriorityQuota"/>
     public static IVostokThrottlingConfigurator UsePriorityQuota(this IVostokThrottlingConfigurator configurator, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
         configurator
-            .ConfigureMiddleware(settings => settings.AddPriorityProperty = true)
-            .UsePropertyQuota(WellKnownThrottlingProperties.Priority, quotaOptionsProvider);
+            .ConfigureMiddleware(settings => settings.UsePriorityQuota())
+            .ConfigureBuilder(builder => builder.UsePriorityQuota(quotaOptionsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseMethodQuota"/>
     public static IVostokThrottlingConfigurator UseMethodQuota(this IVostokThrottlingConfigurator configurator, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
         configurator
-            .ConfigureMiddleware(settings => settings.AddMethodProperty = true)
-            .UsePropertyQuota(WellKnownThrottlingProperties.Method, quotaOptionsProvider);
+            .ConfigureMiddleware(settings => settings.UseMethodQuota())
+            .ConfigureBuilder(builder => builder.UseMethodQuota(quotaOptionsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseUrlQuota"/>
     public static IVostokThrottlingConfigurator UseUrlQuota(this IVostokThrottlingConfigurator configurator, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
         configurator
-            .ConfigureMiddleware(settings => settings.AddUrlProperty = true)
-            .UsePropertyQuota(WellKnownThrottlingProperties.Url, quotaOptionsProvider);
+            .ConfigureMiddleware(settings => settings.UseUrlQuota())
+            .ConfigureBuilder(builder => builder.UseUrlQuota(quotaOptionsProvider));
 
     /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseCustomPropertyQuota"/>
     public static IVostokThrottlingConfigurator UseCustomPropertyQuota(this IVostokThrottlingConfigurator configurator, string propertyName, Func<HttpContext, string> propertyValueProvider, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
         configurator
-            .ConfigureMiddleware(settings => settings.AdditionalProperties.Add(context => (propertyName, propertyValueProvider(context))))
-            .UsePropertyQuota(propertyName, quotaOptionsProvider);
+            .ConfigureMiddleware(settings => settings.UseCustomPropertyQuota(propertyName, propertyValueProvider))
+            .ConfigureBuilder(builder => builder.UseCustomPropertyQuota(propertyName, quotaOptionsProvider));
 }
---
+++ Vostok.Hosting.AspNetCore/Web/Configuration/ThrottlingConfigurationBuilderExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Applications.AspNetCore.Builders;
+using Vostok.Throttling;
+using Vostok.Throttling.Config;
+using Vostok.Throttling.Quotas;
+
+namespace Vostok.Hosting.AspNetCore.Web.Configuration;
+
+[PublicAPI]
+public static class ThrottlingConfigurationBuilderExtensions
+{
+    /// <inheritdoc cref="IVostokThrottlingBuilder.UseEssentials"/>
+    public static ThrottlingConfigurationBuilder UseEssentials(this ThrottlingConfigurationBuilder builder, Func<ThrottlingEssentials> essentialsProvider) =>
+        builder.SetEssentials(essentialsProvider);
+    
+    /// <inheritdoc cref="IVostokThrottlingBuilder.UsePropertyQuota"/>
+    public static ThrottlingConfigurationBuilder UsePropertyQuota(this ThrottlingConfigurationBuilder builder, string propertyName, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.SetPropertyQuota(propertyName, quotaOptionsProvider);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilder.UseCustomQuota"/>
+    public static ThrottlingConfigurationBuilder UseCustomQuota(this ThrottlingConfigurationBuilder builder, IThrottlingQuota quota) =>
+        builder.AddCustomQuota(quota);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseConsumerQuota"/>
+    public static ThrottlingConfigurationBuilder UseConsumerQuota(this ThrottlingConfigurationBuilder builder, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.UsePropertyQuota(WellKnownThrottlingProperties.Consumer, quotaOptionsProvider);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UsePriorityQuota"/>
+    public static ThrottlingConfigurationBuilder UsePriorityQuota(this ThrottlingConfigurationBuilder builder, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.UsePropertyQuota(WellKnownThrottlingProperties.Priority, quotaOptionsProvider);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseMethodQuota"/>
+    public static ThrottlingConfigurationBuilder UseMethodQuota(this ThrottlingConfigurationBuilder builder, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.UsePropertyQuota(WellKnownThrottlingProperties.Method, quotaOptionsProvider);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseUrlQuota"/>
+    public static ThrottlingConfigurationBuilder UseUrlQuota(this ThrottlingConfigurationBuilder builder, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.UsePropertyQuota(WellKnownThrottlingProperties.Url, quotaOptionsProvider);
+
+    /// <inheritdoc cref="IVostokThrottlingBuilderExtensions.UseCustomPropertyQuota"/>
+    public static ThrottlingConfigurationBuilder UseCustomPropertyQuota(this ThrottlingConfigurationBuilder builder, string propertyName, Func<PropertyQuotaOptions> quotaOptionsProvider) =>
+        builder.SetPropertyQuota(propertyName, quotaOptionsProvider);
+}
